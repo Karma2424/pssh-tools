ab0283df38f2098f6f406f328b400e3fca57bb8c
Karma2424 <58363516+Karma2424@users.noreply.github.com>
2023-12-24 16:44:37 +0530

    annoying

--- src/lib/pssh/widevine.ts
+++ src/lib/pssh/widevine.ts
@@ -18,11 +18,9 @@ const getPsshData = (request: T.WidevineDataEncodeConfig) => {
   const root = protobuf.loadSync(protoFile)
 
   const WidevineCencHeader = root.lookupType('proto.WidevineCencHeader')
-  const payload: WidevineProtoPayload = {
-    algorithm: 0
-  }
+  const payload: WidevineProtoPayload = {}
   if (request.algorithm) {
-    payload.algorithm = 1 // 0: Unencrypted - 1: AESCTR
+    payload.algorithm = request.algorithm // 0: Unencrypted - 1: AESCTR
   }
   if (request.keyIds && request.keyIds.length > 0) {
     const keyIdsBuffer = request.keyIds.map((key) => {
